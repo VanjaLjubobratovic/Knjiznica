1550ae1183b0802c7610c90c41ec44ddc0d70696
Vanja Ljubobratovic <34680384+VanjaLjubobratovic@users.noreply.github.com>
2019-11-05 11:43:57 +0100

    Initial commit

---
+++ Knjiznica/src/Beletristika.java
@@ -0,0 +1,22 @@
+
+public class Beletristika extends Knjiga implements Posudba {
+
+	@Override
+	public void posudi() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void vrati() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void naruci(int br) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Knjiznica/src/ZnanstKnjiga.java
@@ -0,0 +1,22 @@
+
+public class ZnanstKnjiga extends Knjiga implements Kupnja {
+
+	@Override
+	public void kupi() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setCijena(int cijena) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void naruci(int br) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
